7532ab49de8ca1ce15660a4f3fd42ffba342d608
navneetkumar <navneetc486@gmail.com>
2020-12-28 21:40:12 +0530

    signup page added, firebase added, some refactoring is also done

--- src/index.jsx
+++ src/index.jsx
@@ -1,58 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 //import App from './App';
-import Card from "./Cards";
-import Sdata from "./Sdata";
-import AppFooter from "./footer/Footer";
-  //hello navneet
+import Home from "./Home/Home";
+import Signup from "./Auth/Signup";
+import Login from "./Auth/Login";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+
        
 ReactDOM.render(
-<>
-<h1 className="hedding_style">list of top 6 NETFLIX serise in 2020</h1>
 
-<Card  imgsrc={Sdata[0].imgsrc}
-        sname={Sdata[0].sname}
-       title={Sdata[0].title}
-      
-       link={Sdata[0].link}
-/>
-<Card  imgsrc={Sdata[1].imgsrc}
-        sname={Sdata[1].sname}
-       title={Sdata[1].title}
-      
-       link={Sdata[1].link}
-/>
-<Card  imgsrc={Sdata[2].imgsrc}
-       sname={Sdata[2].sname}
-       title={Sdata[2].title}
-       
-       link={Sdata[2].link}
-/>
-<Card
-   imgsrc={Sdata[3].imgsrc}
-   sname={Sdata[3].sname}
-   title={Sdata[3].title}
-   
-   link={Sdata[3].link}
-/>
-<Card
-    imgsrc={Sdata[4].imgsrc}
-    sname={Sdata[4].sname}
-    title={Sdata[4].title}
-    
-    link={Sdata[4].link}
-/>
-<Card
-   imgsrc={Sdata[5].imgsrc}
-   sname={Sdata[5].sname}
-   title={Sdata[5].title}
-   
-   link={Sdata[5].link}
-/>
-<AppFooter/>
 
-</>
+<BrowserRouter>
+<Switch>
+
+  <Route exact path ="/" component = {Home} />
+  <Route path ="/signup" component = {Signup} />
+  <Route path ="/login" component = {Login} />
+  <Route  component = {Home} />
+</Switch>
+   
+
+
+</BrowserRouter>
 ,
 document.getElementById("root")
 );
